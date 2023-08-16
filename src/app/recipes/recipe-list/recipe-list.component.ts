76ed81c3ab66e751e499f477513a082f49ae8199
Carlos Filipe Cardoso Dias Pereira <carlosfcdp94@hotmail.com>
2023-08-16 23:28:06 +0100

    create list and recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'Test', 'https://outdoors.com/wp-content/uploads/2018/10/Veg-recipes.jpg'),
     new Recipe('A test Recipe', 'Test', 'https://outdoors.com/wp-content/uploads/2018/10/Veg-recipes.jpg')
   ];
 
